7ce7608311dfd3c0587fdd3e73a5b75624b1c08c
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-06-06 20:46:01 -0300

    feat: :children_crossing: add colors variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,15 +13,10 @@ module.exports = {
     colors: {
       //& Surface
       "surface-0": withOpacityValue("--clr-surface-0-alpha"),
-      "surface-1": withOpacityValue("--clr-surface-1-alpha"),
-      "surface-2": withOpacityValue("--clr-surface-2-alpha"),
-      "surface-3": withOpacityValue("--clr-surface-3-alpha"),
       // & Text
       "text-0": withOpacityValue("--clr-text-0-alpha"),
-      "text-1": withOpacityValue("--clr-text-1-alpha"),
-      "text-2": withOpacityValue("--clr-text-2-alpha"),
       // & Brand
-      "brand-0": withOpacityValue("--clr-brand-0-alpha"),
+      "accent-0": withOpacityValue("--clr-brand-0-alpha"),
     },
     extend: {},
   },
